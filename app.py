c41444f9b386df5c25e97058c243ce622f41f1b5
Ramya <ramyaarasu28@gmail.com>
2026-04-21 20:39:15 +0530

    fixed port issue for railway

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ import json
 import random
 import requests
 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Chatbot is running 🚀"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+
+
+
+
+
+
+
+
+
 app = Flask(__name__)
 CORS(app)
 
